0819e72664bf29250e5c70b524f5bcb80a8b648d
VijaiGR <130731306+VijaiGR@users.noreply.github.com>
2023-08-18 09:43:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ import re
 
 
 # SETTING PAGE CONFIGURATIONS
-st.set_page_config(page_title="BizCardX: Extracting Business Card Data with OCR | By Arshad Ayub Ahmed",
+st.set_page_config(page_title="BizCardX: Extracting Business Card Data with OCR",
                    layout="wide",
                    initial_sidebar_state="expanded",
-                   menu_items={'About': """# This OCR app is created by *Arshad Ayub Ahmed*!"""})
+                   menu_items={'About': """# This OCR app is created by *Vijai Gowtham*!"""})
 st.markdown("<h1 style='text-align: center; color: white;'>BizCardX: Extracting Business Card Data with OCR</h1>", unsafe_allow_html=True)
 
 #=========hide the streamlit main and footer
